fa9d60bee040745266a301b2efe26b9b5d927f35
tjysdsg <tjy1018543509@163.com>
2020-02-14 15:37:42 +0800

    Update gtest dependency

--- scripts/install-dep.sh
+++ scripts/install-dep.sh
@@ -3,6 +3,5 @@
 if [ "$NAME" = "ubuntu" ]; then
     sudo apt-get update
     sudo apt-get -y install build-essential cmake libgflags2.2 libgflags-dev
-    ./scripts/install-gtest.sh
     sudo bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)" # install llvm, https://apt.llvm.org/
 fi
