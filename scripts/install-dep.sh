4720c49601dc48df8ec643b122189c9bbb8075e1
tjysdsg <tjy1018543509@163.com>
2020-02-27 19:50:38 +0800

    Fix github workflow and trivis-ci

--- scripts/install-dep.sh
+++ scripts/install-dep.sh
@@ -2,6 +2,9 @@
 
 if [ "$NAME" = "ubuntu" ]; then
     sudo apt-get update
-    sudo apt-get -y install build-essential cmake libgflags2.2 libgflags-dev
+    sudo apt-get -y install build-essential cmake
     sudo bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)" # install llvm, https://apt.llvm.org/
 fi
+
+git submodule init
+git submodule update --recursive
