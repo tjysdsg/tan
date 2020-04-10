7a11a29b67afee52e863d4d2618dddfae8cf295d
tjysdsg <tjy1018543509@163.com>
2020-04-10 19:29:58 +0800

    Fix wrong order of code blocks of loop and if

--- src/ast/ast_loop.cpp
+++ src/ast/ast_loop.cpp
@@ -8,15 +8,25 @@ llvm::Value *ASTLoop::codegen(CompilerSession *compiler_session) {
   if (_loop_type == ASTLoopType::WHILE) {
     Function *func = compiler_session->get_builder()->GetInsertBlock()->getParent();
     BasicBlock *loop_bb = BasicBlock::Create(*compiler_session->get_context(), "loop", func);
-    BasicBlock *body_bb = BasicBlock::Create(*compiler_session->get_context(), "loop_body", func);
-    BasicBlock *after_bb = BasicBlock::Create(*compiler_session->get_context(), "after_loop", func);
 
     /// enter loop_bb
     compiler_session->get_builder()->CreateBr(loop_bb);
 
-    /// start insertion in loop_bb
-    compiler_session->get_builder()->SetInsertPoint(loop_bb);
+    /// loop body
+    BasicBlock *body_bb = BasicBlock::Create(*compiler_session->get_context(), "loop_body", func);
+    compiler_session->get_builder()->SetInsertPoint(body_bb);
+    _children[1]->codegen(compiler_session);
+
+    /// back to loop start
+    compiler_session->get_builder()->CreateBr(loop_bb);
+
+    /// after loop
+    BasicBlock *after_bb = BasicBlock::Create(*compiler_session->get_context(), "after_loop", func);
+
     /// condition
+    /// to make sure that the last code block is after_bb, after_bb is created after loop body is generated
+    /// since the call to CreateCondBr should depends on after_bb, this is generated last
+    compiler_session->get_builder()->SetInsertPoint(loop_bb);
     // TODO: use ASTTy::convert_to
     auto *cond = _children[0]->codegen(compiler_session);
     unsigned condition_bits = cond->getType()->getPrimitiveSizeInBits();
@@ -29,14 +39,7 @@ llvm::Value *ASTLoop::codegen(CompilerSession *compiler_session) {
     }
     compiler_session->get_builder()->CreateCondBr(cond, body_bb, after_bb);
 
-    /// loop body
-    compiler_session->get_builder()->SetInsertPoint(body_bb);
-    _children[1]->codegen(compiler_session);
-
-    /// back to loop start
-    compiler_session->get_builder()->CreateBr(loop_bb);
-
-    /// after loop
+    /// done
     compiler_session->get_builder()->SetInsertPoint(after_bb);
     compiler_session->get_builder()->CreateCall(Intrinsic::GetIntrinsic(IntrinsicType::NOOP, compiler_session));
   } else {
