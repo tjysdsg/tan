352b27fe85b6a505e0d2831643835429cabd6eb1
tjysdsg <tjy1018543509@163.com>
2020-04-22 17:51:52 +0800

    Update release build script

--- scripts/compile-release.sh
+++ scripts/compile-release.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+export CC=$(which clang)
+export CXX=$(which clang++)
 mkdir -p build
 pushd build
 cmake .. -DCMAKE_BUILD_TYPE=Release || exit 1
 make -j8 || exit 1
 popd
 
+# artifact
 mkdir -p tan-release
 mkdir -p tan-release/include/tan
 cp -r lib tan-release/
