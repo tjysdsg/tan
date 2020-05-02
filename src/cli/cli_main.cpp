24d6f104a7a5f3f2814dd77ab92fef0415d3d12c
tjysdsg <tjy1018543509@163.com>
2020-05-02 17:25:09 +0800

    Refactor CLI

--- src/cli/cli_main.cpp
+++ src/cli/cli_main.cpp
@@ -10,17 +10,20 @@ DEFINE_string(l, "", "files to link against");
 
 int cli_main(int *pargc, char ***pargv) {
   gflags::SetUsageMessage("tan compiler");
+  gflags::SetVersionString(std::to_string(TAN_VERSION_MAJOR) + "." + std::to_string(TAN_VERSION_MINOR) + "."
+      + std::to_string(TAN_VERSION_PATCH));
   gflags::ParseCommandLineFlags(pargc, pargv, true);
+
+  /// source files
   std::vector<char *> files;
   files.reserve((size_t) *pargc);
-  for (int i = 1; i < *pargc; ++i) {
-    files.push_back((*pargv)[i]);
-  }
+  for (int i = 1; i < *pargc; ++i) { files.push_back((*pargv)[i]); }
+  /// files to link to
   std::vector<std::string> link_files = flag_to_list(FLAGS_l);
   std::vector<const char *> link_files_c;
-  for (const auto &lf : link_files) {
-    link_files_c.push_back(lf.c_str());
-  }
+  for (const auto &lf : link_files) { link_files_c.push_back(lf.c_str()); }
+
+  /// build config
   TanCompilation config;
   config.type = EXE;
   config.out_file = FLAGS_o.c_str();
@@ -32,18 +35,15 @@ int cli_main(int *pargc, char ***pargv) {
     config.n_link_files = link_files_c.size();
     config.link_files = link_files_c.data();
   }
-  if (FLAGS_type == "obj") {
-    config.type = OBJ;
-  } else if (FLAGS_type == "slib") {
-    config.type = SLIB;
-  } else if (FLAGS_type == "dlib") {
-    config.type = DLIB;
-  }
-  if (FLAGS_print_ast) {
-    config.verbose = 2;
-  } else if (FLAGS_print_ir_code) {
-    config.verbose = 1;
-  }
+
+  /// output type
+  if (FLAGS_type == "obj") { config.type = OBJ; }
+  else if (FLAGS_type == "slib") { config.type = SLIB; }
+  else if (FLAGS_type == "dlib") { config.type = DLIB; }
+
+  /// verbosity
+  if (FLAGS_print_ast) { config.verbose = 2; }
+  else if (FLAGS_print_ir_code) { config.verbose = 1; }
   compile_files((unsigned) files.size(), files.data(), &config);
   return 0;
 }
