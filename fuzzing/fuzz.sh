24ac7595beb2af00690b979567cbe3818d65078d
tjysdsg <tjy1018543509@163.com>
2020-05-18 21:30:30 +0800

    Update fuzzing script

--- fuzzing/fuzz.sh
+++ fuzzing/fuzz.sh
@@ -1,21 +1,28 @@
 #!/usr/bin/env bash
 
-export AFL_USE_UBSAN=1
-export AFL_USE_ASAN=1
-export CC=$(which afl-clang-fast)
-export CXX=$(which afl-clang-fast++)
-export LD=$(which afl-clang-fast)
-
 # prepare test files
 mkdir -p test_input
 cp -r ../src/test/test_src/* test_input/
 
 # build
+export AFL_USE_UBSAN=1
+export AFL_USE_ASAN=1
 mkdir -p build
 pushd build
+
+export CC=$(which afl-clang-fast)
+export CXX=$(which afl-clang-fast++)
+export LD=$(which afl-clang-fast)
 cmake ../.. -DLIB_OUTPUT_DIR=./lib -DEXE_OUTPUT_DIR=./bin -DENABLE_CCACHE=OFF
-make -j6 tanc runtime
+make -j6 tanc
+
 popd
 
 # fuzz!
 afl-fuzz -i test_input/ -o out -m none -f test_case.tan -- ./build/bin/tanc test_case.tan -I.. -lruntime -L../runtime -o a.out
+
+unset AFL_USE_UBSAN
+unset AFL_USE_ASAN
+unset CC
+unset CXX
+unset LD
