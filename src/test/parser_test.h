ec903530a280a81dfa1d3e8f4b5cf80f847e6fd5
tjysdsg <tjy1018543509@163.com>
2021-04-28 09:39:01 +0800

    Fix parser test

--- src/test/parser_test.h
+++ src/test/parser_test.h
@@ -4,6 +4,7 @@
 #include "lexer.h"
 #include "token.h"
 #include "compiler_session.h"
+#include "compiler.h"
 #include "reader.h"
 #include "parser.h"
 #include "src/ast/ast_node.h"
@@ -12,6 +13,7 @@
 
 using tanlang::Reader;
 using tanlang::Parser;
+using tanlang::Compiler;
 using tanlang::CompilerSession;
 using tanlang::tokenize;
 using tanlang::TokenType;
@@ -24,25 +26,8 @@ ASTNodePtr parse_string(str code) {
   auto result = tokenize(&reader);
   auto tokens = tokenize(&reader);
 
-  llvm::TargetMachine *tm = nullptr;
-  { // FIXME: remove this
-    llvm::InitializeAllTargetInfos();
-    llvm::InitializeAllTargets();
-    llvm::InitializeAllTargetMCs();
-    llvm::InitializeAllAsmParsers();
-    llvm::InitializeAllAsmPrinters();
-    auto target_triple = llvm::sys::getDefaultTargetTriple();
-    str error;
-    auto target = llvm::TargetRegistry::lookupTarget(target_triple, error);
-    auto CPU = "generic";
-    auto features = "";
-    llvm::TargetOptions opt;
-    /// relocation model
-    auto RM = llvm::Reloc::Model::PIC_;
-    tm = target->createTargetMachine(target_triple, CPU, features, opt, RM);
-  }
-
-  CompilerSession *cs = new CompilerSession("test module", tm);
+  Compiler compiler("test module"); // FIXME: decouple parsing and compiling
+  CompilerSession *cs = new CompilerSession("test module", Compiler::GetDefaultTargetMachine());
   auto *parser = new Parser(tokens, "test", cs);
   return parser->parse();
 }
@@ -53,7 +38,10 @@ TEST(parser, function_decl) {
              "  return 666;"
              "}";
   auto node = parse_string(code);
-  EXPECT_EQ(ASTType::FUNC_DECL, node->_type);
+  EXPECT_EQ(1, node->_children.size());
+  auto statement = node->_children[0];
+  EXPECT_EQ(1, statement->_children.size());
+  EXPECT_EQ(ASTType::FUNC_DECL, statement->_children[0]->_type);
 }
 
 #endif /* TAN_LEXER_TEST_H */
