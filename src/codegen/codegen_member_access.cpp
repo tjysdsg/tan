020f18a2a84faa86906c7a006160262f6208acf5
tjysdsg <tjy1018543509@163.com>
2021-05-31 00:49:28 +0800

    Fix codegen of member access

--- src/codegen/codegen_member_access.cpp
+++ src/codegen/codegen_member_access.cpp
@@ -1,49 +1,49 @@
 #include "src/codegen/code_generator_impl.h"
 #include "src/llvm_include.h"
 #include "src/ast/ast_type.h"
+#include "src/ast/expr.h"
 #include "compiler_session.h"
 
 using namespace tanlang;
 
-Value *CodeGeneratorImpl::codegen_member_access(const ASTMemberAccessPtr &p) {
+Value *CodeGeneratorImpl::codegen_member_access(const MemberAccessPtr &p) {
   auto *builder = _cs->_builder;
   set_current_debug_location(p);
-  auto lhs = p->get_child_at<ASTNode>(0);
-  ASTNodePtr rhs = nullptr;
-  if (p->get_children_size() >= 2) {
-    rhs = p->get_child_at<ASTNode>(1);
-  } /// pointer access only have 1 child node
+
+  auto lhs = p->get_lhs();
+  auto rhs = p->get_rhs();
+
   auto *from = codegen(lhs);
   Value *ret;
   switch (p->_access_type) {
-    case MemberAccessType::MemberAccessBracket: {
-      if (lhs->_type->_is_lvalue) { from = builder->CreateLoad(from); }
+    case MemberAccess::MemberAccessBracket: {
+      if (lhs->get_type()->_is_lvalue) { from = builder->CreateLoad(from); }
       auto *rhs_val = codegen(rhs);
-      if (rhs->_type->_is_lvalue) { rhs_val = builder->CreateLoad(rhs_val); }
+      if (rhs->get_type()->_is_lvalue) { rhs_val = builder->CreateLoad(rhs_val); }
       ret = builder->CreateGEP(from, rhs_val, "bracket_access");
       break;
     }
-    case MemberAccessType::MemberAccessMemberVariable: {
-      if (lhs->_type->_is_lvalue && lhs->_type->_is_ptr && _h.get_contained_ty(lhs->_type)) {
+    case MemberAccess::MemberAccessMemberVariable: {
+      if (lhs->get_type()->_is_lvalue && lhs->get_type()->_is_ptr && _h.get_contained_ty(lhs->get_type())) {
         /// auto dereference pointers
         from = builder->CreateLoad(from);
       }
       ret = builder->CreateStructGEP(from, (unsigned) p->_access_idx, "member_variable");
       break;
     }
-    case MemberAccessType::MemberAccessDeref:
+    case MemberAccess::MemberAccessDeref:
       ret = builder->CreateLoad(from);
       break;
-    case MemberAccessType::MemberAccessMemberFunction:
-      ret = codegen(p->get_child_at<ASTNode>(1));
+    case MemberAccess::MemberAccessMemberFunction:
+      ret = codegen(rhs);
       break;
-    case MemberAccessType::MemberAccessEnumValue:
+    case MemberAccess::MemberAccessEnumValue:
       ret = nullptr;
       // TODO: codegen of enum_type.enum_value
       break;
     default:
       TAN_ASSERT(false);
   }
-  p->_llvm_value = ret;
-  return ret;
+
+  return p->_llvm_value = ret;
 }
