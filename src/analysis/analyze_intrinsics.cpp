826e6c0c44a1d20884bf4b6210d40a4889b3d6e5
tjysdsg <tjy1018543509@163.com>
2021-05-31 22:27:05 +0800

    Fix intrinsic analysis

--- src/analysis/analyze_intrinsics.cpp
+++ src/analysis/analyze_intrinsics.cpp
@@ -12,14 +12,24 @@ using namespace tanlang;
 
 void AnalyzerImpl::analyze_intrinsic(const ASTBasePtr &_p) {
   auto p = ast_must_cast<Intrinsic>(_p);
-  auto pc = p->get_sub();
-  auto c = ast_must_cast<ASTNamed>(pc);
+  auto c = p->get_sub();
 
   /// name
-  p->set_name(c->get_name());
+  str name;
+  switch (c->get_node_type()) {
+    case ASTNodeType::FUNC_CALL:
+      name = ast_must_cast<FunctionCall>(c)->get_name();
+      break;
+    case ASTNodeType::ID:
+      name = ast_must_cast<Identifier>(c)->get_name();
+      break;
+    default:
+      TAN_ASSERT(false);
+      break;
+  }
 
   /// search for the intrinsic type
-  auto q = Intrinsic::intrinsics.find(c->get_name());
+  auto q = Intrinsic::intrinsics.find(name);
   if (q == Intrinsic::intrinsics.end()) {
     report_error(p, "Invalid intrinsic");
   }
@@ -43,18 +53,18 @@ void AnalyzerImpl::analyze_intrinsic(const ASTBasePtr &_p) {
     }
     case IntrinsicType::GET_DECL: {
       p->set_type(ASTType::Create(_cs, Ty::STRING));
-      if (pc->get_node_type() != ASTNodeType::STRING_LITERAL) {
-        report_error(pc, "Expect a string argument");
+      if (c->get_node_type() != ASTNodeType::STRING_LITERAL) {
+        report_error(c, "Expect a string argument");
       }
       // TODO: set p->_value to the source code of p
       break;
     }
     case IntrinsicType::COMP_PRINT: {
       p->set_type(void_type);
-      if (pc->get_node_type() != ASTNodeType::STRING_LITERAL) {
+      if (c->get_node_type() != ASTNodeType::STRING_LITERAL) {
         report_error(p, "Invalid call to compprint, one argument with type 'str' required");
       }
-      std::cout << fmt::format("Message ({}): {}\n", _h.get_source_location(p), c->get_name());
+      std::cout << fmt::format("Message ({}): {}\n", _h.get_source_location(p), name);
       break;
     }
     default:
