472a63c58171442b97a4ffae92865ddc436385f0
tjysdsg <tjy1018543509@163.com>
2021-06-04 01:21:22 +0800

    Use raw pointers for anything

--- src/base/container.h
+++ src/base/container.h
@@ -10,24 +10,14 @@
 template<typename T> using vector = std::vector<T>;
 using str = std::string;
 template<typename T1, typename T2> using umap = std::unordered_map<T1, T2>;
-template<typename T> using ptr = T *;
 
+// TODO: use raw pointer directly
+//  There's no need to allow switching to shared_ptr
+template<typename T> using ptr = T *;
+template<typename T> inline ptr<T> make_ptr() { return new T; }
+template<typename To, typename From> ptr<To> cast_ptr(ptr<From> p) { return dynamic_cast<To *>(p); }
 template<typename T, typename... Args> inline ptr<T> make_ptr(Args &&... args) {
   return new T(std::forward<Args>(args)...);
 }
-
-template<typename T> inline ptr<T> make_ptr() {
-  return new T;
-}
-
-template<typename To, typename From> ptr<To> cast_ptr(ptr<From> p) {
-  #ifdef DEBUG
-  return dynamic_cast<To *>(p);
-  #else
-  return reinterpret_cast<To *>(p);
-  #endif
-}
-
-template<typename T> using enable_ptr_from_this = std::enable_shared_from_this<T>;
 
 #endif //__TAN_SRC_BASE_CONTAINER_H__
