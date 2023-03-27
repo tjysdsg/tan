bba58a1614414ea72b5084c0bc6b03743ad20db0
tjysdsg <tjy1018543509@163.com>
2023-03-27 04:21:46 -0400

    Fix C++ concept compile error for some compilers

--- include/common/compiler_action.h
+++ include/common/compiler_action.h
@@ -5,9 +5,18 @@
 
 namespace tanlang {
 
+template <class T, class U>
+concept SameHelper = std::is_same_v<T, U>;
+
+/**
+ * \brief Some compilers don't have std::same_as
+ */
+template <class T, class U>
+concept same_as = SameHelper<T, U> && SameHelper<U, T>;
+
 template <typename C, typename Input, typename Output>
 concept HasImpl = requires(C c, Input input) {
-                    { c.run_impl(input) } -> std::same_as<Output>;
+                    { c.run_impl(input) } -> same_as<Output>;
                   };
 
 template <typename Derived, typename Input, typename Output> class CompilerAction : public ASTVisitor<Derived> {
