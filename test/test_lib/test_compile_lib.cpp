b1fedd7babcf544b242c8654d7fdc456532b598e
tjysdsg <tjy1018543509@163.com>
2023-04-09 21:02:34 -0400

    Add static library compilation tests

--- test/test_lib/test_compile_lib.cpp
+++ test/test_lib/test_compile_lib.cpp
@@ -5,6 +5,9 @@
 #include "cli/cli.h"
 #include "base.h"
 #include <gtest/gtest.h>
+#include <filesystem>
+
+namespace fs = std::filesystem;
 
 #ifndef TANC_PATH
 #error "Define TANC_PATH before compiling this"
@@ -19,21 +22,28 @@
 class MyFixture : public ::testing::Test {};
 
 class TestCompileLib : public MyFixture {
+private:
+  bool _shared = false;
+
 public:
-  TestCompileLib() {}
+  TestCompileLib(bool shared) : _shared(shared) {}
 
-  void TestBody() override {
-    // compile into a static library
-    vector<const char *> cmd{__STR__(TANC_PATH),
-                             "-I" __STR__(TAN_PROJECT_SOURCE_DIR),
-                             "-L" __STR__(TAN_PROJECT_SOURCE_DIR) "/runtime",
-                             "-lruntime",
-                             "-shared",
-                             "-o",
-                             "libtest.so",
-                             __STR__(TAN_TEST_SOURCE_DIR) "/lib1.tan",
-                             __STR__(TAN_TEST_SOURCE_DIR) "/lib2.tan"};
+  void build_lib() {
+    // remove previous outputs
+    fs::remove(fs::path(__STR__(TAN_PROJECT_SOURCE_DIR) "/libtest.so"));
+    fs::remove(fs::path(__STR__(TAN_PROJECT_SOURCE_DIR) "/libtest.a"));
 
+    vector<const char *> cmd{
+        __STR__(TANC_PATH),
+        "-I" __STR__(TAN_PROJECT_SOURCE_DIR),
+        "-L" __STR__(TAN_PROJECT_SOURCE_DIR) "/runtime",
+        "-lruntime",
+        _shared ? "-shared" : "-static",
+        "-o", //
+        _shared ? __STR__(TAN_PROJECT_SOURCE_DIR) "/libtest.so" : __STR__(TAN_PROJECT_SOURCE_DIR) "/libtest.a",
+        __STR__(TAN_TEST_SOURCE_DIR) "/lib1.tan",
+        __STR__(TAN_TEST_SOURCE_DIR) "/lib2.tan",
+    };
     for (auto *c : cmd) {
       std::cout << c << " ";
     }
@@ -42,15 +52,18 @@ public:
     auto *argv = (char **)cmd.data();
     ASSERT_EQ(cli_main(argc, argv), 0);
   }
+
+  void TestBody() override { build_lib(); }
 };
 
-void register_tanc_test() {
-  ::testing::RegisterTest("tanc_test_fixture", "lib_test", nullptr, "", __FILE__, __LINE__,
-                          [=]() -> MyFixture * { return new TestCompileLib(); });
-}
-
+// usage: test_compile_lib true/false
 int main(int argc, char **argv) {
+  assert(argc == 2);
   ::testing::InitGoogleTest(&argc, argv);
-  register_tanc_test();
+
+  bool shared_lib = str(argv[1]) == "true";
+  ::testing::RegisterTest("tanc_test_fixture", "lib_test", nullptr, "", __FILE__, __LINE__,
+                          [=]() -> MyFixture * { return new TestCompileLib(shared_lib); });
+
   return RUN_ALL_TESTS();
 }
